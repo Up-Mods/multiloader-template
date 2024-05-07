30a8dadf71020d42e3985a5bf73ad5abe01d2ab4
Up <10714589+UpcraftLP@users.noreply.github.com>
2024-05-07 06:47:28 +0200

    set up for 1.20.1

--- Quilt/src/main/java/dev/upcraft/examplemod/quilt/entrypoints/Client.java
+++ Quilt/src/main/java/dev/upcraft/examplemod/quilt/entrypoints/Client.java
@@ -1,10 +1,8 @@
 package dev.upcraft.examplemod.quilt.entrypoints;
 
-import dev.upcraft.sparkweave.api.annotation.CalledByReflection;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
 
-@CalledByReflection
 public class Client implements ClientModInitializer {
 
     @Override
