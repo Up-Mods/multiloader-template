8d8c0abba501bc56b859464701fa9c6afce29df2
Up <10714589+UpcraftLP@users.noreply.github.com>
2024-10-04 07:10:13 +0200

    update to Minecraft 1.21.1

---
+++ Fabric/src/main/java/dev/upcraft/examplemod/fabric/entrypoints/Client.java
@@ -0,0 +1,13 @@
+package dev.upcraft.examplemod.fabric.entrypoints;
+
+import dev.upcraft.sparkweave.api.annotation.CalledByReflection;
+import net.fabricmc.api.ClientModInitializer;
+
+@CalledByReflection
+public class Client implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
