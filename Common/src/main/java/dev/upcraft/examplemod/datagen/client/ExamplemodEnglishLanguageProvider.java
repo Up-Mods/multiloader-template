53b4cc494ad2ae2a5ca7e753acba51d7b6f744cf
Up <10714589+UpcraftLP@users.noreply.github.com>
2025-04-18 17:01:26 +0200

    add data generation example

---
+++ Common/src/main/java/dev/upcraft/examplemod/datagen/client/ExamplemodEnglishLanguageProvider.java
@@ -0,0 +1,21 @@
+package dev.upcraft.examplemod.datagen.client;
+
+import dev.upcraft.sparkweave.api.datagen.ContextAwarePackOutput;
+import dev.upcraft.sparkweave.api.datagen.TranslationBuilder;
+import dev.upcraft.sparkweave.api.datagen.provider.SparkweaveLanguageProvider;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.locale.Language;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ExamplemodEnglishLanguageProvider extends SparkweaveLanguageProvider {
+
+    public ExamplemodEnglishLanguageProvider(ContextAwarePackOutput output, CompletableFuture<HolderLookup.Provider> registriesFuture) {
+        super(output, registriesFuture, Language.DEFAULT);
+    }
+
+    @Override
+    public void generateTranslations(HolderLookup.Provider registries, TranslationBuilder builder) {
+        builder.add("examplemod.title", "Example Mod");
+    }
+}
