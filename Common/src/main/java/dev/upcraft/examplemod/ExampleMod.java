bd9710f444ea5f5ab439ed9b65a6110335e5aa45
Up <10714589+UpcraftLP@users.noreply.github.com>
2025-04-18 15:31:17 +0200

    add autoservice annotations

--- Common/src/main/java/dev/upcraft/examplemod/ExampleMod.java
+++ Common/src/main/java/dev/upcraft/examplemod/ExampleMod.java
@@ -1,9 +1,11 @@
 package dev.upcraft.examplemod;
 
+import com.google.auto.service.AutoService;
 import dev.upcraft.sparkweave.api.entrypoint.MainEntryPoint;
 import dev.upcraft.sparkweave.api.platform.ModContainer;
 import net.minecraft.resources.ResourceLocation;
 
+@AutoService(MainEntryPoint.class)
 public class ExampleMod implements MainEntryPoint {
     public static final String MOD_ID = "examplemod";
 
--- Common/src/main/java/dev/upcraft/examplemod/ExampleModClient.java
+++ Common/src/main/java/dev/upcraft/examplemod/ExampleModClient.java
@@ -1,8 +1,10 @@
 package dev.upcraft.examplemod;
 
+import com.google.auto.service.AutoService;
 import dev.upcraft.sparkweave.api.entrypoint.ClientEntryPoint;
 import dev.upcraft.sparkweave.api.platform.ModContainer;
 
+@AutoService(ClientEntryPoint.class)
 public class ExampleModClient implements ClientEntryPoint {
 	@Override
 	public void onInitializeClient(ModContainer mod) {
